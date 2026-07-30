f1b16c59ac786596739a6b64ae41475bfafd76f1
Mega-Bits <80045566+Mega-Bits@users.noreply.github.com>
2026-07-30 09:22:45 +0200

    Apply channel-points response guard

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 __version__ = "2.0.5"
+
+from .channel_points_context_patch import apply_patch as _apply_channel_points_context_patch
+
+_apply_channel_points_context_patch()
+del _apply_channel_points_context_patch
+
 from .TwitchChannelPointsMiner import TwitchChannelPointsMiner
 
 __all__ = [
