1732f5247d48257142014f8fd9f177c345066ecf
Mega-Bits <80045566+Mega-Bits@users.noreply.github.com>
2026-07-31 14:58:19 +0200

    Enable persistent streak and game Drop patches

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -3,17 +3,26 @@ __version__ = "2.0.5"
 
 from .channel_points_context_patch import apply_patch as _apply_channel_points_context_patch
 from .discord_format_patch import apply_patch as _apply_discord_format_patch
+from .drop_games_patch import apply_patch as _apply_drop_games_patch
+from .drop_games_patch import apply_startup_patch as _apply_drop_games_startup_patch
 from .playback_access_token_patch import apply_patch as _apply_playback_access_token_patch
 from .watch_notifications_patch import apply_patch as _apply_watch_notifications_patch
+from .watch_streak_persistence_patch import apply_patch as _apply_watch_streak_persistence_patch
 
 _apply_channel_points_context_patch()
 _apply_discord_format_patch()
 _apply_playback_access_token_patch()
+_apply_watch_streak_persistence_patch()
+_apply_drop_games_patch()
 _apply_watch_notifications_patch()
+_apply_drop_games_startup_patch()
 del _apply_channel_points_context_patch
 del _apply_discord_format_patch
+del _apply_drop_games_patch
+del _apply_drop_games_startup_patch
 del _apply_playback_access_token_patch
 del _apply_watch_notifications_patch
+del _apply_watch_streak_persistence_patch
 
 from .TwitchChannelPointsMiner import TwitchChannelPointsMiner
 
