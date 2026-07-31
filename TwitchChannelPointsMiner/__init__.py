a053898917c921382ae02b8ec9d1be67e65553be
Mega-Bits <80045566+Mega-Bits@users.noreply.github.com>
2026-07-31 10:26:05 +0200

    Enable compact Discord Markdown formatter

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -2,11 +2,14 @@
 __version__ = "2.0.5"
 
 from .channel_points_context_patch import apply_patch as _apply_channel_points_context_patch
+from .discord_format_patch import apply_patch as _apply_discord_format_patch
 from .playback_access_token_patch import apply_patch as _apply_playback_access_token_patch
 
 _apply_channel_points_context_patch()
+_apply_discord_format_patch()
 _apply_playback_access_token_patch()
 del _apply_channel_points_context_patch
+del _apply_discord_format_patch
 del _apply_playback_access_token_patch
 
 from .TwitchChannelPointsMiner import TwitchChannelPointsMiner
