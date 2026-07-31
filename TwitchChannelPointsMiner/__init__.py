d39d0dc92a7bc8776db712b6701a974a4dbb00ac
Mega-Bits <80045566+Mega-Bits@users.noreply.github.com>
2026-07-31 11:45:19 +0200

    Enable startup and watch notifications

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -4,13 +4,16 @@ __version__ = "2.0.5"
 from .channel_points_context_patch import apply_patch as _apply_channel_points_context_patch
 from .discord_format_patch import apply_patch as _apply_discord_format_patch
 from .playback_access_token_patch import apply_patch as _apply_playback_access_token_patch
+from .watch_notifications_patch import apply_patch as _apply_watch_notifications_patch
 
 _apply_channel_points_context_patch()
 _apply_discord_format_patch()
 _apply_playback_access_token_patch()
+_apply_watch_notifications_patch()
 del _apply_channel_points_context_patch
 del _apply_discord_format_patch
 del _apply_playback_access_token_patch
+del _apply_watch_notifications_patch
 
 from .TwitchChannelPointsMiner import TwitchChannelPointsMiner
 
