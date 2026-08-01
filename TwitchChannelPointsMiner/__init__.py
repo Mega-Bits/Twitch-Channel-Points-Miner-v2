727c84f60b238f615a9746b572526f1a8332ebb1
Mega-Bits <80045566+Mega-Bits@users.noreply.github.com>
2026-08-01 13:00:02 +0200

    Enable SQLite status history

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -8,6 +8,7 @@ from .drop_games_patch import apply_patch as _apply_drop_games_patch
 from .drop_games_patch import apply_startup_patch as _apply_drop_games_startup_patch
 from .playback_access_token_patch import apply_patch as _apply_playback_access_token_patch
 from .status_dashboard_patch import apply_patch as _apply_status_dashboard_patch
+from .status_history_patch import apply_patch as _apply_status_history_patch
 from .watch_notifications_patch import apply_patch as _apply_watch_notifications_patch
 from .watch_streak_persistence_patch import apply_patch as _apply_watch_streak_persistence_patch
 
@@ -20,6 +21,7 @@ _apply_drop_game_main_list_preference_patch()
 _apply_watch_notifications_patch()
 _apply_drop_games_startup_patch()
 _apply_status_dashboard_patch()
+_apply_status_history_patch()
 del _apply_channel_points_context_patch
 del _apply_discord_format_patch
 del _apply_drop_game_main_list_preference_patch
@@ -27,6 +29,7 @@ del _apply_drop_games_patch
 del _apply_drop_games_startup_patch
 del _apply_playback_access_token_patch
 del _apply_status_dashboard_patch
+del _apply_status_history_patch
 del _apply_watch_notifications_patch
 del _apply_watch_streak_persistence_patch
 
