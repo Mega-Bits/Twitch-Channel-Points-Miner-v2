a1bd022427c473243efb1cfdf8472688af38f7f9
Mega-Bits <80045566+Mega-Bits@users.noreply.github.com>
2026-08-02 09:22:02 +0200

    Remove startup notification patch activation

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -5,7 +5,6 @@ from .channel_points_context_patch import apply_patch as _apply_channel_points_c
 from .discord_format_patch import apply_patch as _apply_discord_format_patch
 from .drop_game_main_list_preference_patch import apply_patch as _apply_drop_game_main_list_preference_patch
 from .drop_games_patch import apply_patch as _apply_drop_games_patch
-from .drop_games_patch import apply_startup_patch as _apply_drop_games_startup_patch
 from .playback_access_token_patch import apply_patch as _apply_playback_access_token_patch
 from .status_dashboard_patch import apply_patch as _apply_status_dashboard_patch
 from .status_dashboard_webhook_patch import apply_patch as _apply_status_dashboard_webhook_patch
@@ -20,7 +19,6 @@ _apply_watch_streak_persistence_patch()
 _apply_drop_games_patch()
 _apply_drop_game_main_list_preference_patch()
 _apply_watch_notifications_patch()
-_apply_drop_games_startup_patch()
 _apply_status_dashboard_patch()
 _apply_status_dashboard_webhook_patch()
 _apply_status_dashboard_clock_patch()
@@ -28,7 +26,6 @@ del _apply_channel_points_context_patch
 del _apply_discord_format_patch
 del _apply_drop_game_main_list_preference_patch
 del _apply_drop_games_patch
-del _apply_drop_games_startup_patch
 del _apply_playback_access_token_patch
 del _apply_status_dashboard_clock_patch
 del _apply_status_dashboard_patch
